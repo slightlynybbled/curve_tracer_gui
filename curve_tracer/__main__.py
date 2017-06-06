9f0f93b9f3b7db315ea27d7df9e50fc9d05fc473
Jason R. Jones <slightlynybbled@gmail.com>
2017-06-06 15:48:03 -0400

    Refactored for easier installability

--- curve_tracer/__main__.py
+++ curve_tracer/__main__.py
@@ -1,14 +1,3 @@
-import tkinter as tk
 from curve_tracer.app import CurveTracer
 
-root = tk.Tk()
-root.resizable(0, 0)
-root.title("Curve Tracer - for(embed)")
-
-try:
-    root.iconbitmap('curve_tracer/images/forembed.ico')
-except FileNotFoundError:
-    pass
-
-app = CurveTracer(root)
-root.mainloop()
+ct = CurveTracer()
